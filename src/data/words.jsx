24cc8d5f74c358c76d0def1932d235eee40558dc
erick-amorim377 <erickamorim221@gmail.com>
2025-09-24 13:43:14 -0300

    Fixing mobile responsiveness.
    Fixing database words

--- src/data/words.jsx
+++ src/data/words.jsx
@@ -1,14 +1,14 @@
 export const wordsList = {
-    car: ["Engine", "Door", "Hood", "Tire", "Steering Wheel", "Gasoline", "Oil"],
+    car: ["Engine", "Door", "Hood", "Tire", "clutch", "Gasoline", "Oil"],
     Fruits: ["apple", "banana", "pear", "papaya", "orange", "watermelon", "strawberry"],
     Body: ["arm", "leg", "brain", "neck", "eyes", "nose", "fingers"],
-    Computer: ["keyboard", "mouse", "monitor", "case", "power supply", "ssd", "ram"],
+    Computer: ["keyboard", "mouse", "monitor", "case", "webcam", "ssd", "ram"],
     Programming: ["Fremework", "JavaScript", "React", "PHP", "CSS", "Java", "HTML"],
     Food: ["egg", "steak", "chicken", "stroganoff", "pasta", "lasagna", "bread"],
     Games: ["minecraft", "tetris", "mario", "zelda", "overwatch", "sonic", "pokemon"],
-    Drink: ["beer", "wine", "water", "juice", "Coffee", "Soda", "Caipirinha"],
+    Drink: ["beer", "wine", "water", "juice", "Coffee", "Soda", "Daiquiri"],
     nature: ["tree", "cloud", "grass", "insect", "earth", "river", "flowers", "star"],
-    school: ["pencil", "pen", "pencil case", "notebook", "eraser", "board", "teacher"],
+    school: ["pencil", "pen", "book", "notebook", "eraser", "board", "teacher"],
     animals: ["duck", "horse", "dog", "cat", "monkey", "giraffe", "bear", "penguin"],
-    tool: ["Philips", "screwdriver", "drill", "Makita", "tape measure", "screwdriver", "saw"]
+    tool: ["Philips", "pliers", "drill", "Makita", "tape", "saw", "hammer"]
 }
